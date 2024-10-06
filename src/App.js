55a11dd2e6b7f44c5d9b35e920ab34bf72a3c18f
Thufir Hawat <hawat@Bartoszs-MacBook-Pro.local>
2024-10-06 11:56:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import FC from './FirstComponent.js';
+import PLO from './GameOfLife.js';
+
+const whaaaat = <PLO height={10} width ={10}/>;
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+     
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {whaaaat}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
