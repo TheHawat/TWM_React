a397ff8d7a22d70ceaa837a696e25839ed145fc5
Thufir Hawat <hawat@Bartoszs-MacBook-Pro.local>
2024-10-08 22:06:40 +0200

    Please commit branch

--- src/Cell.js
+++ src/Cell.js
@@ -1,4 +1,7 @@
+import life from './cell_life.png'
+import dead from './cell_dead.png'
+
 export default function FirstComponent(state){
-    if (state === 1) return (<button>1</button>)
-    return(<button>0</button>)
+    if (state === 1) return (<input type="image" height='40px' src={life}></input>)
+    return(<input type="image" height='40px' src={dead}></input>)
 }
